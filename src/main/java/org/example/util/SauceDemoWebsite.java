5b8b14254b053b8a5bdb562d4f3bf5b933c0d8f0
Mednikarov <Kaloyan.Mednikarov@adastragrp.com>
2024-05-14 16:48:11 +0300

    Added SauceDemoWebsite

---
+++ src/main/java/org/example/util/SauceDemoWebsite.java
@@ -0,0 +1,10 @@
+package org.example.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SauceDemoWebsite {
+    private void getSelenium(){
+        WebDriver driver = new ChromeDriver();
+    }
+}
