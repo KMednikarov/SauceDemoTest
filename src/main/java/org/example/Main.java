dadb85bfb78a4ba19e2d72bfb6326ad742f03880
Mednikarov <Kaloyan.Mednikarov@adastragrp.com>
2024-05-14 20:39:50 +0300

    Added login and add items to cart functionalities

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.utils.SauceDemoWebsite;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SauceDemoWebsite sauceDemoWebsite = new SauceDemoWebsite();
+        sauceDemoWebsite.login("standard_user", "secret_sauce");
+        String url = sauceDemoWebsite.getCurrentUrl();
+        System.out.println(url);
+        sauceDemoWebsite.addItemsToCart(2);
     }
 }
---
+++ src/main/java/org/example/web/XPaths.java
@@ -0,0 +1,5 @@
+package org.example.web;
+
+public class XPaths {
+    public static final String ADD_TO_CART_BTN = "/html/body/div/div/div/div[2]/div/div/div//div[2]/div[2]/button";
+}
